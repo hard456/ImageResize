abf18f129c79928459ebcce12ffb7b498b5ea1fe
hard456 <janpalcut@seznam.cz>
2017-09-24 00:35:31 +0200

    image resize

---
+++ src/main/java/jpalcut/imageprocessing/controller/Controller.java
@@ -0,0 +1,76 @@
+package jpalcut.imageprocessing.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import jpalcut.imageprocessing.ImageResize;
+
+import java.io.File;
+import java.util.List;
+
+public class Controller {
+
+    @FXML
+    public TextField widthField, heightField;
+    @FXML
+    public Label imageCount, statusLabel;
+    @FXML
+    public Button resizeImages, selectImages;
+
+    List<File> fileList;
+
+    public void selectImages() {
+        //Filter file type
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("All files", "*.jpg", "*.jpeg");
+
+        //Initialization fileChooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //List of files
+        fileList = fileChooser.showOpenMultipleDialog(null);
+
+        //Show number of chosen files
+        if (fileList == null) {
+            imageCount.setText("No images");
+            resizeImages.setDisable(true);
+            statusLabel.setText("Choose files");
+        }
+        else {
+            if (fileList.size() == 1) {
+                imageCount.setText("1 image");
+            } else {
+                imageCount.setText(fileList.size() + " images");
+            }
+            resizeImages.setDisable(false);
+            statusLabel.setText("");
+        }
+    }
+
+    public void resizeImages(){
+
+        selectImages.setDisable(true);
+        resizeImages.setDisable(true);
+        widthField.setDisable(true);
+        heightField.setDisable(true);
+
+        //New width and height
+        int width = 0, height = 0;
+
+        try {
+            width = Integer.parseInt(widthField.getText());
+            height = Integer.parseInt(heightField.getText());
+        }
+        catch (Exception e){
+            //Catch the wrong size of image
+            statusLabel.setText("Fill the image size");
+        }
+        ImageResize resize = new ImageResize(fileList, width, height);
+        resize.start();
+
+    }
+
+}
